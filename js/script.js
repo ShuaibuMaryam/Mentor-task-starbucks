e4525d87088ea2de8e2a607b086d99b74822e85d
ShuaibuMaryam <shuaibumaryam113@gmail.com>
2021-06-17 19:17:19 -0700

    continuation

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 //-----favourites tab-----//
-function openTab(event, tabName){
-    var i, tabContent, tabLink;
-    tabContent = document.getElementsByClassName("fav-tab-content");
-    for(i = 0; i < tabContent.length; i++){
-        tabContent[i].getElementsByClassName.display = "none";
-    }
-    tabLink = document.getElementsByClassName("tablink");
-    for(i = 0; i < tabLink.length; i++){
-        tabLink[i].className.replace("active", "");
-        console.log('clicked')
-    }
+// function openTab(event, tabName){
+//     var i, tabContent, tabLink;
+//     tabContent = document.getElementsByClassName("fav-tab-content");
+//     for(i = 0; i < tabContent.length; i++){
+//         tabContent[i].getElementsByClassName.display = "none";
+//     }
+//     tabLink = document.getElementsByClassName("tablink");
+//     for(i = 0; i < tabLink.length; i++){
+//         tabLink[i].className.replace("active", "");
+//         console.log('clicked')
+//     }
 
-    document.getElementById(tabName).style.display = "flex";
-    event.currentTarget.className += "active";
-}
+//     document.getElementById(tabName).style.display = "flex";
+//     event.currentTarget.className += "active";
+// }
