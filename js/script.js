001d54edc8c21285aaf38db347cc88fe5bb9c2b2
ShuaibuMaryam <shuaibumaryam113@gmail.com>
2021-06-21 18:45:19 -0700

    media queries

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 //-----Toggleable nav-----//
-
+function navToggle() {
+    let nav = document.getElementById("navigationToggle");
+    nav.classList.toggle('toggle')
+    
+}
 
 
 //-----favourites tab-----//
-function tabToggle(event, allContent){
+function tabToggle(event, allContent) {
     let tabContent, tabList, i;
     tabContent = document.getElementsByClassName("tab-content");
     tabList = document.getElementsByClassName("tablist");
-    for(i = 0; i < tabContent.length; i++){
+    for (i = 0; i < tabContent.length; i++) {
         tabContent[i].style.display = "none";
     }
-    for(i = 0; i < tabList.length; i++){
+    for (i = 0; i < tabList.length; i++) {
         tabList[i].className = tabList[i].className.replace(" active", "");
     }
     document.getElementById(allContent).style.display = "flex";
